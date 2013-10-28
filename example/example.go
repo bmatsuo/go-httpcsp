ded02367e28286cce5e0ae64a9084530147b21b6
Bryan Matsuo <bryan.matsuo@gmail.com>
2013-10-27 19:24:15 -0700

    expand on example server

--- example/example.go
+++ example/example.go
@@ -11,17 +11,25 @@ func main() {
 	addr := ":8000"
 	fmt.Println("Serving requests at http://localhost" + addr)
 
+	// define security policies with a chaining api.
 	csp := httpcsp.New().
 		DefaultSrc(httpcsp.SELF).
 		ScriptSrc(httpcsp.NONE).
 		ImgSrc(httpcsp.SELF).
-		ReportURI("/policy/violation").
-		MustCompile()
+		ReportURI("/policy/violation")
+	fmt.Println("GLOBAL POLICY:", csp)
 
-	fmt.Println("Content-Security-Policy:", csp)
+	// extend existing policies for localized exceptions to the global policy.
+	cspinline := csp.ScriptSrc(httpcsp.UNSAFE_INLINE)
+	fmt.Println("UNSAFE EXCEPTION:", csp)
 
-	http.Handle("/", csp.Middleware(http.HandlerFunc(Root)))
+	base := http.NewServeMux()
+	base.Handle("/", csp.MustCompile().Middleware(http.HandlerFunc(Root)))
+	base.Handle("/danger-zone", cspinline.MustCompile().Middleware(http.HandlerFunc(Root)))
 
+	http.Handle("/", base)
+
+	// the violation handler doesn't serve HTML and does not need CSP headers.
 	http.Handle("/policy/violation", httpcsp.ViolationHandler(CSPViolation))
 
 	http.ListenAndServe(addr, http.DefaultServeMux)
